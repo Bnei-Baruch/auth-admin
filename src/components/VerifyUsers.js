0e4de7eb662533a045d42c0d87135239e342712f
amnonbb <amnonbb@gmail.com>
2020-05-17 15:17:11 +0300

    Show ignored users

--- src/components/VerifyUsers.js
+++ src/components/VerifyUsers.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import {Container, Segment, Table, Icon, Menu, Popup, Label} from "semantic-ui-react";
+import {Container, Segment, Table, Icon, Menu, Popup, Label, Divider} from "semantic-ui-react";
 import {getAuthData} from "../shared/tools";
-import {AUTH_API} from "../shared/env";
+import {AUTH_API, IGNORED_ID} from "../shared/env";
 
 class VerifyUsers extends Component {
 
     state = {
         users: [],
+        ignored_users: [],
         selected_user: "",
         disabled: true,
         loading: true,
@@ -17,6 +18,9 @@ class VerifyUsers extends Component {
         getAuthData(`${AUTH_API}/vusers`, (users) => {
             this.setState({users, loading: false});
         });
+        getAuthData(`${AUTH_API}/users/${IGNORED_ID}`, (ignored_users) => {
+            this.setState({ignored_users});
+        });
     };
 
     selectUser = (user) => {
@@ -25,7 +29,7 @@ class VerifyUsers extends Component {
     };
 
     render() {
-        const {users,selected_user,loading} = this.state;
+        const {users,selected_user,loading,ignored_users} = this.state;
 
         let users_content = users.map(user => {
             const {id,firstName,lastName,emailVerified,email,attributes} = user;
@@ -50,6 +54,25 @@ class VerifyUsers extends Component {
             )
         })
 
+        let users_ignored = ignored_users.map(user => {
+            const {id,firstName,lastName,email,createdTimestamp,attributes} = user;
+            const request = attributes && attributes.request !== undefined;
+            const req_user = request ? attributes.request[0] : "";
+            const reg_time = new Date(createdTimestamp).toUTCString();
+            return (
+                <Table.Row key={id}
+                           active={id === selected_user}
+                           negative={true}
+                           onClick={() => this.selectUser(user)} >
+                    <Table.Cell>{email}</Table.Cell>
+                    <Table.Cell>{firstName}</Table.Cell>
+                    <Table.Cell>{lastName}</Table.Cell>
+                    <Table.Cell>{reg_time}</Table.Cell>
+                    <Table.Cell><b>{req_user}</b></Table.Cell>
+                </Table.Row>
+            )
+        })
+
         return (
             <Container fluid >
                 <Menu size='large' secondary>
@@ -69,6 +92,19 @@ class VerifyUsers extends Component {
                     </Menu.Menu>
                 </Menu>
                 <Segment textAlign='center' className="group_list" raised loading={loading}>
+                    <Table selectable compact='very' basic structured className="admin_table" unstackable>
+                        <Table.Body>
+                            <Table.Row disabled>
+                                <Table.Cell width={2}>Email</Table.Cell>
+                                <Table.Cell width={2}>First Name</Table.Cell>
+                                <Table.Cell width={2}>Last Name</Table.Cell>
+                                <Table.Cell width={2}>Reg Time</Table.Cell>
+                                <Table.Cell width={1}>Ignored by</Table.Cell>
+                            </Table.Row>
+                            {users_ignored}
+                        </Table.Body>
+                    </Table>
+                    <Divider />
                     <Table selectable compact='very' basic structured className="admin_table" unstackable>
                         <Table.Body>
                             <Table.Row disabled>
--- src/shared/env.js
+++ src/shared/env.js
@@ -1,2 +1,3 @@
 export const AUTH_API = process.env.REACT_APP_AUTH_API_BACKEND;
-export const PENDING_ID = "c46f3890-fa01-4933-968d-488ba5ca3153"
+export const PENDING_ID = "c46f3890-fa01-4933-968d-488ba5ca3153";
+export const IGNORED_ID = "4111c55c-1931-4ca1-9f6f-127963d40dcd";
